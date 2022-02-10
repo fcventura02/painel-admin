ae900c5e63a3a85e2281cf09642d2b503ba7d1b4
filipe.cristiano <filipe.cristiano@uaify.com>
2022-02-10 13:39:35 -0300

    Added icons and new pages

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/template/Layout";
+
+export default function Settings() {
+  return (
+    <Layout
+      titulo="Ajustes & configurações"
+      subtitulo="Deixe o sistema com sua cara"
+    >
+      <h1>Teste conteudo</h1>
+    </Layout>
+  );
+}
---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/template/Layout";
+
+export default function Notification() {
+  return (
+    <Layout
+      titulo="Notificações"
+      subtitulo="Fique por dentro do que tem acontecido"
+    >
+      <h1>Teste conteudo</h1>
+    </Layout>
+  );
+}
