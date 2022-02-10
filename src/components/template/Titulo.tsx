da121aaae77637bbd31122f01fefd57cadddafa4
filipe.cristiano <filipe.cristiano@uaify.com>
2022-02-10 13:43:05 -0300

    updated style title

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -1,18 +1,25 @@
-interface ITitulo{
-    titulo:string,
-    subtitulo:string,
+interface ITitulo {
+  titulo: string;
+  subtitulo: string;
 }
 
-
-export default function Titulo({titulo, subtitulo}: ITitulo){
-    return(
-        <div>
-            <h1 className={`
-            
-            `}>{titulo}</h1>
-            <h2 className={`
-            
-            `}>{subtitulo}</h2>
-        </div>
-    )
+export default function Titulo({ titulo, subtitulo }: ITitulo) {
+  return (
+    <div>
+      <h1
+        className={`
+            font-black text-3xl text-gray-900
+            `}
+      >
+        {titulo}
+      </h1>
+      <h2
+        className={`
+        font-light text-sm text-gray-600
+            `}
+      >
+        {subtitulo}
+      </h2>
+    </div>
+  );
 }
